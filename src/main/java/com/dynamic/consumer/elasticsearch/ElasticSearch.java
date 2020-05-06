7408678f2996b047cb6ca96b084434bdc2803e8a
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-05-06 17:01:17 -0300

    treating exception on elasticsearch client creation

--- src/main/java/com/dynamic/consumer/elasticsearch/ElasticSearch.java
+++ src/main/java/com/dynamic/consumer/elasticsearch/ElasticSearch.java
@@ -9,37 +9,46 @@ import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-
 @Component
 public class ElasticSearch {
-	
+
+	static Logger logger = LoggerFactory.getLogger(ElasticSearch.class.getName());
+
 	@Value("${elasticsearch.username.credential}")
 	private String usernameCredential;
-	
+
 	@Value("${elasticsearch.password.credential}")
 	private String passwordCredential;
-	
+
 	@Value("${elasticsearch.hostnameCloud}")
 	private String hostnameCloud;
 
 	public RestHighLevelClient createClient() {
+		RestHighLevelClient client = null;
+		try {
+			final CredentialsProvider credentialProvider = new BasicCredentialsProvider();
+			credentialProvider.setCredentials(AuthScope.ANY,
+					new UsernamePasswordCredentials(usernameCredential, passwordCredential));
 
-		final CredentialsProvider credentialProvider = new BasicCredentialsProvider();
-		credentialProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(
-				usernameCredential, passwordCredential));
+			RestClientBuilder clientBuilder = RestClient.builder(new HttpHost(hostnameCloud, 443, "https"))
+					.setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
 
-		RestClientBuilder clientBuilder = RestClient.builder(new HttpHost(hostnameCloud, 443, "https"))
-				.setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+						public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+							return httpClientBuilder.setDefaultCredentialsProvider(credentialProvider);
+						}
+					});
 
-					public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
-						return httpClientBuilder.setDefaultCredentialsProvider(credentialProvider);
-					}
-				});
+			client = new RestHighLevelClient(clientBuilder);
+			logger.info("ElasticSearch client created");
+		} catch (Exception e) {
+			logger.error("Error creating ElasticSearch client.", e.getMessage());
+		}
 
-		RestHighLevelClient client = new RestHighLevelClient(clientBuilder);
 		return client;
 	}
 }
