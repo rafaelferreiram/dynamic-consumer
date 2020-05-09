9b24768669bbdce958ab63d132579d94cf80f2a2
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-05-09 17:43:16 -0300

    removing unused code

--- src/main/java/com/dynamic/consumer/kafka/service/impl/KafkaConsumerServiceImpl.java
+++ src/main/java/com/dynamic/consumer/kafka/service/impl/KafkaConsumerServiceImpl.java
@@ -71,7 +71,6 @@ public class KafkaConsumerServiceImpl implements KafkaConsumerService {
 					try {
 						Thread.sleep(100);
 					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
 				}
