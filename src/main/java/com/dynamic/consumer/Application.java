34bdf1c42402ee092840afe0a43b785517e7b88a
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-05-10 18:30:32 -0300

    adding swagger to project

--- src/main/java/com/dynamic/consumer/Application.java
+++ src/main/java/com/dynamic/consumer/Application.java
@@ -2,9 +2,16 @@ package com.dynamic.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 @EnableAsync
 public class Application {
 
@@ -12,4 +19,9 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.dynamic.consumer.controller")).build();
+	}
 }
