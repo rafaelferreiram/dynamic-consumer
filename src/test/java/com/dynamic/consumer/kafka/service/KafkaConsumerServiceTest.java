987eaa7d985927dc06c9996a7876fa08123bf064
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-05-13 18:02:59 -0300

    Initiating service unit test

---
+++ src/test/java/com/dynamic/consumer/kafka/service/KafkaConsumerServiceTest.java
@@ -0,0 +1,9 @@
+package com.dynamic.consumer.kafka.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class KafkaConsumerServiceTest {
+
+}
