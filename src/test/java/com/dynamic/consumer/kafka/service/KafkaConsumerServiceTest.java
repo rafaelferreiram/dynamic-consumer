8649ac68a76c88a2f0e24cc0f898b02c4dcc50df
rafael.ferreira <rafah.26ferreira@hotmail.com>
2020-08-10 21:42:41 -0300

    removing unit

--- src/test/java/com/dynamic/consumer/kafka/service/KafkaConsumerServiceTest.java
+++
@@ -1,9 +0,0 @@
-package com.dynamic.consumer.kafka.service;
-
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-public class KafkaConsumerServiceTest {
-
-}
